205c264eb81db5c87eb835c26b1658771f9af85b
EmilVHermansen <emil.valbak.hermansen@gmail.com>
2020-03-24 08:58:32 +0100

    Added interfaces

---
+++ src/main/java/dk/cphbusiness/banking/CustomerManager.java
@@ -0,0 +1,109 @@
+package dk.cphbusiness.banking;
+
+import java.util.List;
+
+public interface CustomerManager {
+    CustomerDetail getCustomer(String cpr);
+
+
+    public static class CustomerIdentifier {
+        private final long id;
+
+        public CustomerIdentifier(long id) {
+            this.id = id;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+    }
+
+    /**
+     * Member details
+     */
+    public static class CustomerDetail extends CustomerIdentifier {
+        private String cpr, name;
+        private BankManager.BankSummary bank;
+        private List<String> accountNumbers;
+
+        public CustomerDetail(long id, String cpr, String name, BankManager.BankSummary bank, List<String> accountNumbers) {
+            super(id);
+            this.cpr = cpr;
+            this.name = name;
+            this.bank = bank;
+            this.accountNumbers = accountNumbers;
+        }
+
+        public String getCpr() {
+            return cpr;
+        }
+
+        public void setCpr(String cpr) {
+            this.cpr = cpr;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public BankManager.BankSummary getBank() {
+            return bank;
+        }
+
+        public void setBank(BankManager.BankSummary bank) {
+            this.bank = bank;
+        }
+
+        public List<String> getAccountNumbers() {
+            return accountNumbers;
+        }
+
+        public void setAccountNumbers(List<String> accountNumbers) {
+            this.accountNumbers = accountNumbers;
+        }
+    }
+
+    /**
+     * Member summary
+     */
+    public static class CustomerSummary extends CustomerIdentifier {
+        private String cpr, name;
+        private BankManager.BankSummary bank;
+
+        public CustomerSummary(long id, String cpr, String name, BankManager.BankSummary bank) {
+            super(id);
+            this.cpr = cpr;
+            this.name = name;
+            this.bank = bank;
+        }
+
+        public String getCpr() {
+            return cpr;
+        }
+
+        public void setCpr(String cpr) {
+            this.cpr = cpr;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public BankManager.BankSummary getBank() {
+            return bank;
+        }
+
+        public void setBank(BankManager.BankSummary bank) {
+            this.bank = bank;
+        }
+    }
+}
